b91b3571edd8786b52fdbd76a030192127111c6a
vivekboyina <vivekboyina77@gmail.com>
2025-12-09 20:23:53 +0530

    Time: 509 ms (85.77%) | Memory: 343.2 MB (81.02%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        unordered_map<int,int> mp, ma;
+        const long long MOD = 1000000007;
+
+        for(int i : nums) mp[i]++;
+
+        long long cnt = 0;
+
+        for(int i : nums)
+        {
+            if(mp.find(i*2) != mp.end() && ma.find(i*2) != ma.end())
+            {
+                if(i == 0)
+                    cnt = (cnt + ( (1LL * ma[i*2] * (mp[i*2] - ma[i*2] - 1)) % MOD )) % MOD;
+                else
+                    cnt = (cnt + ( (1LL * ma[i*2] * (mp[i*2] - ma[i*2])) % MOD )) % MOD;
+            }
+
+            ma[i]++;
+        }
+
+        return (int)(cnt % MOD);
+    }
+};
