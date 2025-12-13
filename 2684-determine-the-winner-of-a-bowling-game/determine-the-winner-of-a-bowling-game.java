60ef92f6bfe2d4a0ddd6853c2890bd5950ed7f8c
vivekboyina <vivekboyina77@gmail.com>
2025-12-13 11:13:34 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (47.90%) - LeetSync

---
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int isWinner(int[] p1, int[] p2) {
+        int a1 = 0,a2 = 0;
+        for(int i = 0; i < p1.length; i++)
+        {
+            if(i == 0)
+            {
+                a1+=p1[i];
+                a2+=p2[i];
+            }
+            else if(i == 1)
+            {
+                if(p1[0] == 10) a1+=(2*p1[i]);
+                else a1+=p1[i];
+                if(p2[0] == 10) a2+=(2*p2[i]);
+                else a2+=p2[i];
+            }
+            else
+            {
+                if(p1[i - 1] == 10 || p1[i - 2] == 10) a1+=(2*p1[i]);
+                else a1+=p1[i];
+                if(p2[i - 1] == 10 || p2[i - 2] == 10) a2+=(2*p2[i]);
+                else a2+=p2[i];
+            }
+        }
+        if(a1 == a2) return 0;
+        else if(a1 > a2) return 1;
+        return 2;
+    }
+}
