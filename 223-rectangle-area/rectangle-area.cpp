ba5cbec48bde45a5a0aa9ab40fb8acbba2ac7b19
vivekboyina <vivekboyina77@gmail.com>
2025-12-16 11:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (37.12%) - LeetSync

--- 223-rectangle-area/rectangle-area.cpp
+++ 223-rectangle-area/rectangle-area.cpp
@@ -5,23 +5,8 @@ public:
         int w1 = min(ay1,by1),w2 = max(ay2,by2);
         int a1 = abs(ax1 - ax2)*abs(ay1 - ay2),a2 = abs(bx1 - bx2)*abs(by1 - by2);
         int l = 0,w = 0;
-        for(int i = l1; i <= l2; i++)
-        {
-            if(i >= ax1 && i <= ax2 && i >= bx1 && i <= bx2)
-            {
-                cout << i << " ";
-                l+=1;
-            }
-        }
-        cout << endl;
-        for(int i = w1; i <= w2; i++)
-        {
-            if(i >= ay1 && i <= ay2 && i >= by1 && i <= by2)
-            {
-                cout << i << " ";
-                w+=1;
-            }
-        }
+        for(int i = l1; i <= l2; i++) if(i >= ax1 && i <= ax2 && i >= bx1 && i <= bx2) l+=1;
+        for(int i = w1; i <= w2; i++) if(i >= ay1 && i <= ay2 && i >= by1 && i <= by2) w +=1;
         if(l > 0 && w > 0) return (a1 + a2 - (l - 1)*(w - 1));
         return a1 + a2;
     }
