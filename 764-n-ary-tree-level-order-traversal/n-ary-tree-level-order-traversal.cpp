ea8b3efcce27976b3f2e7021d7c7edfb188cc726
vivekboyina <vivekboyina77@gmail.com>
2025-12-25 09:21:21 +0530

    Time: 13 ms (58.06%) | Memory: 16.2 MB (17.63%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>>ans;
+        if(root == nullptr) return ans;
+        queue<Node*>q;
+        q.push(root);
+        ans.push_back({root -> val});
+        while(!q.empty())
+        {
+            int n = q.size();
+            vector<int>r;
+            for(int i = 0; i < n; i++)
+            {
+                Node *t = q.front();
+                for(Node* j : t -> children)
+                {
+                    q.push(j);
+                    r.push_back(j -> val);
+                }
+                q.pop();
+            }
+            if(r.size() > 0) ans.push_back(r);
+        }
+        return ans;
+    }
+};
