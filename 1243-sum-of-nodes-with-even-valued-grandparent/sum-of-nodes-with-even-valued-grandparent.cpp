b9e3e23cb4b3d25ad55a40d4fa24620b5832bbc4
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2025-12-27 20:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (88.29%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    int sumEvenGrandparent(TreeNode* root) {
-        int ans = 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
+    int ans = 0;
+    void dfs(TreeNode* root)
+    {
+        if(root == nullptr) return;
+        if(root -> val % 2 == 0)
         {
-            int n = q.size();
-            for(int i = 0; i < n; i++)
+            if(root -> left != nullptr)
             {
-                TreeNode *t = q.front();
-                if(t -> left != nullptr)
-                {
-                    if(t -> val % 2 == 0)
-                    {
-                        if(t -> left -> right != nullptr) ans+=(t -> left -> right -> val);
-                        if(t -> left -> left != nullptr) ans+=(t -> left -> left -> val);
-                    }
-                    q.push(t -> left);
-                }
-                if(t -> right != nullptr)
-                {
-                    if(t -> val % 2 == 0)
-                    {
-                        if(t -> right -> right != nullptr) ans+=(t -> right -> right -> val);
-                        if(t -> right -> left != nullptr) ans+=(t -> right -> left -> val);
-                    }
-                    q.push(t -> right);
-                }
-                q.pop();
+                if(root -> left -> left != nullptr) ans+=(root -> left -> left -> val);
+                if(root -> left -> right != nullptr) ans+=(root -> left -> right -> val);
+            }
+            if(root -> right != nullptr)
+            {
+                if(root -> right -> left != nullptr) ans+=(root -> right -> left -> val);
+                if(root -> right -> right != nullptr) ans+=(root -> right -> right -> val);
             }
         }
+        dfs(root -> left);
+        dfs(root -> right);
+    }
+    int sumEvenGrandparent(TreeNode* root) {
+        dfs(root);
         return ans;
     }
 };
