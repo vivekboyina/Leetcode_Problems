6ca116046ca69bf59d31a49cdc2af46e4ba38a80
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-01-02 11:41:42 +0530

    Time: 49 ms (5.78%) | Memory: 22.2 MB (17.91%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        stack<int>st;
+        st.push(ast[0]);
+        int i = 1;
+        while(i < ast.size())
+        {
+            if(ast[i] < 0 && !st.empty() && st.top() > 0)
+            {
+                if(!st.empty() && abs(ast[i]) >= st.top())
+                {
+                    while(!st.empty() && st.top() > 0 && st.top() < abs(ast[i])) st.pop();
+                    if(!st.empty() && st.top() == abs(ast[i])) st.pop();
+                    else if(st.empty() || st.top() < 0) st.push(ast[i]);
+                }
+                cout << ast[i] << endl;
+            }
+            else st.push(ast[i]);
+            i+=1;
+        }
+        vector<int>ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
