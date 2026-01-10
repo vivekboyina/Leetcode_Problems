bf4ef11021fd5a7915e91048d3a9cfebb8e6abef
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-01-10 10:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (41.44%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n == 0) return 0;
+        vector<int>dp(n + 1);
+        dp[0] = 0,dp[1] = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            if(i*2 <= n) dp[i*2] = dp[i];
+            if(i*2 + 1 <= n) dp[i*2 + 1] = dp[i] + dp[i + 1];
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
