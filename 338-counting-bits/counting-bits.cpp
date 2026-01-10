ef6bbe7635275ae3396fb592a0d77b785619e5c8
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-01-10 10:53:43 +0530

    Time: 25 ms (5.58%) | Memory: 17.4 MB (7.98%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -6,9 +6,6 @@ public:
         for(int i = 0; i <= n; i++)
         {
             s = bitset<32>(i).to_string();
-            auto it = s.find('1');
-            if(it != string::npos) s = s.substr(it);
-            else s = "0";
             ans[i] = count(s.begin(),s.end(),'1');
         }
         return ans;
