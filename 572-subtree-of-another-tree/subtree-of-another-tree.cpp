3f9d00df172b746372bc97f1b8f7d12a734382d2
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-02-20 10:20:32 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (70.52%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool ans;
+    void check(TreeNode* root,TreeNode* sub)
+    {
+        if(!ans) return;
+        if(root == nullptr && sub == nullptr) return;
+        if(!root || !sub || root -> val != sub -> val)
+        {
+            ans = false;
+            return;
+        }
+        check(root -> left,sub -> left);
+        check(root -> right,sub -> right);
+    }
+    void dfs(TreeNode* root,TreeNode* sub)
+    {
+        if(!root) return;
+        if(root -> val == sub -> val && ans == false) 
+        {
+            ans = true;
+            check(root,sub);
+        }
+        dfs(root -> left,sub);
+        dfs(root -> right,sub);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* sub) {
+        ans = false;
+        dfs(root,sub);
+        return ans;
+    }
+};
