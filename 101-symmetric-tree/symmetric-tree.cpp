4ec4852a6541ccb398e36157b9e55c6668f4242a
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-02-23 09:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (85.13%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool ans = true;
+    void dfs(TreeNode* a,TreeNode* b)
+    {
+        if(a == nullptr && b == nullptr) return;
+        if(ans == false) return;
+        if(a == nullptr || b == nullptr || a -> val != b -> val)
+        {
+            ans = false;
+            return;
+        }
+        dfs(a -> left,b -> right);
+        dfs(a -> right,b -> left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root -> left == nullptr && root -> right == nullptr) return ans;
+        dfs(root -> left,root -> right);
+        return ans;
+    }
+};
