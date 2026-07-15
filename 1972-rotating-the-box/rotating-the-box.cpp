8acba5cbd52b7296c3cd69b4b2f84425fc0f3570
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-15 12:39:03 +0530

    Time: 14 ms (48.23%) | Memory: 60.9 MB (7.30%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& bg) {
+        int lst = 0;
+        int m = bg.size();
+        int n = bg[0].size();
+        int k;
+        for(int i = 0; i < m; i++)
+        {
+            lst = 0;
+            for(int j = 0; j < n; j++)
+            {
+                if(bg[i][j] == '*')
+                {
+                    // cout << "Entered1 : " << j << endl;
+                    k = j - 1;
+                    while(lst < k)
+                    {
+                        if(bg[i][k] == '.' && bg[i][lst] == '#')
+                        {
+                            swap(bg[i][k],bg[i][lst]);
+                            k--;
+                            lst++;
+                        }
+                        else if(bg[i][k] == '.' && bg[i][lst] == '.') lst++;
+                        else if(bg[i][k] == '#' && bg[i][lst] == '#') k--;
+                        else
+                        {
+                            lst++;
+                            k--;
+                        }
+                    }
+                    lst = j + 1;
+                }
+                else if(j == n - 1)
+                {
+                    // cout << "Entered2 : "  << n - 1 << endl;
+                    k = j;
+                    while(lst < k)
+                    {
+                        if(bg[i][k] == '.' && bg[i][lst] == '#')
+                        {
+                            swap(bg[i][k],bg[i][lst]);
+                            k--;
+                            lst++;    
+                        }
+                        else if(bg[i][k] == '.' && bg[i][lst] == '.') lst++;
+                        else if(bg[i][k] == '#' && bg[i][lst] == '#') k--;
+                        else
+                        {
+                            lst++;
+                            k--;
+                        }
+                    }
+                    lst = j;
+                }
+            }
+        }
+        vector<vector<char>>ans;
+        for(int i = 0; i < n; i++)
+        {
+            vector<char>row;
+            for(int j = m - 1; j >= 0; j--) row.push_back(bg[j][i]);
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
