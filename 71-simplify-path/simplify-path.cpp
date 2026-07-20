ab55eaa989c525614c5799b8d04d9c584c88f0a5
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-20 20:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (65.18%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,75 +1,23 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string s = "";
-        stack<string>st;
-        int cnt = 0;
-        for(char i : path)
+        vector<string>st;
+        string s;
+        for(char i : path + "/")
         {
             if(i == '/')
             {
-                if(cnt > 0 && s.length() <= cnt)
-                {
-                    if(cnt > 2)
-                    {
-                        st.push(s);
-                        st.push("/");
-                    }
-                    else if(cnt == 2)
-                    {
-                        if(!st.empty()) st.pop();
-                        if(!st.empty()) st.pop();
-                    }
-                    s = "";
-                    cnt = 0;
-                }
-                if(st.empty()) st.push("/");
-                else if(s == "") continue;
-                else
-                {
-                    st.push(s);
-                    st.push("/");
-                }
-                s = "";
-                cnt = 0;
-            }
-            else if(i == '.')
-            {
-                cnt+=1;
-                s+='.';
+                if(s == ".."){ if(!st.empty()) st.pop_back();}
+                else if(s != "." && !s.empty()) st.push_back(s);
+                s.clear();
             }
             else s+=i;
         }
-        if(cnt > 0 && s.length() <= cnt)
-        {
-            if(cnt > 2)
-            {
-                st.push(s);
-                st.push("/");
-            }
-            else if(cnt == 2)
-            {
-                if(!st.empty()) st.pop();
-                if(!st.empty()) st.pop();
-            }
-            s = "";
-            cnt = 0;
-        }
         string ans = "";
-        if(s.length() > 0) st.push(s);
-        stack<string>ts;
-        while(!st.empty())
+        for(int i = 0; i < st.size(); i++)
         {
-            ts.push(st.top());
-            st.pop();
+            ans += "/" + st[i];
         }
-        while(!ts.empty())
-        {
-            ans+=ts.top();
-            ts.pop();
-        }
-        if(ans.length() > 1 && ans[ans.size() - 1] == '/') ans.pop_back();
-        if(ans == "") ans = "/";
-        return ans;
+        return ans.empty() ? "/" : ans;
     }
 };
