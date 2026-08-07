f0ea7a28f1d29d7327d78386f30a0d5f5ec3c594
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-07 10:28:22 +0530

    Add Codeforces 2254A - Riptide

---
+++ codeforces/Unrated/2254_A - Riptide/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+        {
+            int a,b,c;
+            cin >> a >> b >> c;
+            cout << min(abs(a - b),min(abs(b - c),abs(a - c))) << endl;
+        }
+}
