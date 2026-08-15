801eab7e7c0bec4780ca9595b12ac14431311e9f
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-15 17:19:10 +0530

    Time: 31 ms (97.61%) | Memory: 11 MB (45.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -6,10 +6,7 @@ public:
         if(i<0||i>=board.size()||j<0||j>=board[0].size()||board[i][j]!=word[in]) return false;
         char temp=board[i][j];
         board[i][j]='#';
-        bool found=rec(i-1,j,in+1,board,word)||
-                   rec(i+1,j,in+1,board,word)||
-                   rec(i,j-1,in+1,board,word)||
-                   rec(i,j+1,in+1,board,word);
+        bool found=rec(i-1,j,in+1,board,word)|| rec(i+1,j,in+1,board,word)|| rec(i,j-1,in+1,board,word)|| rec(i,j+1,in+1,board,word);
         board[i][j]=temp;
         return found;
     }
@@ -19,17 +16,10 @@ public:
         if(word.size()>n*m) return false;
         int bc[256]={};
         int wc[256]={};
-        for(auto& row:board)
-            for(char c:row)
-                bc[c]++;
-        for(char c:word)
-            wc[c]++;
-        for(int i=0;i<256;i++)
-            if(wc[i]>bc[i]) return false;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(board[i][j]==word[0]&&rec(i,j,0,board,word))
-                    return true;
+        for(auto& row:board) for(char c:row) bc[c]++;
+        for(char c:word) wc[c]++;
+        for(int i=0;i<256;i++) if(wc[i]>bc[i]) return false;
+        for(int i=0;i<n;i++) for(int j=0;j<m;j++) if(board[i][j]==word[0]&&rec(i,j,0,board,word)) return true;
         return false;
     }
 };
