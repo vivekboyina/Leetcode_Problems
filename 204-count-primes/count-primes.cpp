da44d7641ca8bfe399ee958b79500fee35926e47
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-25 15:11:41 +0530

    Time: 434 ms (12.18%) | Memory: 8.6 MB (99.59%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,20 +1,23 @@
+vector<bool>prms(5000001,true);
 class Solution {
 public:
-    void sieve(int n,vector<bool>& prms)
+    void sieve()
     {
-        for(int i = 2; i*i < n; i++)
+        for(int i = 2; i*i <= 5000000; i++)
         {
-            if(prms[i] == true) for(int j = i*i; j < n; j+=i) prms[j] = false;
+            if(prms[i] == true) for(int j = i*i; j <= 5000000; j+=i) prms[j] = false;
         }
     }
     int countPrimes(int n) {
         if(n < 2) return 0;
-        vector<bool>prms(n,true);
-        prms[0] = false;
-        prms[1] = false;
-        sieve(n,prms);
-        int ans = 0;
-        for(bool i : prms) if(i) ans++;
+        static bool flag = true;
+        if(flag)
+        {
+            flag = false;
+            sieve();
+        }
+        long long ans = 0;
+        for(int i = 2; i <  n; i++) if(prms[i]) ans+=1;
         return ans;
     }
 };
